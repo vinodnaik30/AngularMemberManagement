5ab05138c775daf5a2d89537b0f6d71938169208
vinodnaik30 <naik.vinni@gmail.com>
2018-10-10 22:20:14 +0530

    Updated package

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MemberManagement'`, () => {
+  it(`should have as title 'membermanage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MemberManagement');
+    expect(app.title).toEqual('membermanage');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MemberManagement!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to membermanage!');
   });
 });
